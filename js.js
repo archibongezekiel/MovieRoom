2e1040840af54f5c3c18f006fd18d09c17cc01be
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-02-07 08:05:02 +0100

    new changes

---
+++ js.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let moviesHead = document.querySelector('.movies-head');
+
+let moviesOption = document.querySelector('#option-1');
+console.log(moviesOption);
+
+moviesOption.addEventListener('', () => {
+  alert('yes');
+});
